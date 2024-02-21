301230aaae37b29784d6dd17d47f01c00cbcd84b
kuangthree <kuangthree@qq.com>
2024-02-21 23:03:44 +0800

    根据无名杀PR分支修改menu.js

--- menu.js
+++ menu.js
@@ -1,11 +1,4 @@
 'use strict';
 decadeModule.import((lib, game, ui, get, ai, _status) => {
-	Mixin.replace(
-		'ui.create.menu',
-		/\s*\/\s*game\s*\.\s*documentZoom\s*/g,
-		'',
-		/\s*get\s*\.\s*is\s*\.\s*phoneLayout\s*\?\s*.*\s*:\s*/,
-		'',
-		/\s*if\s*\(\s*Math\s*\.\s*round\s*\(\s*2\s*\*\s*game\s*\.\s*documentZoom\s*\)\s*<\s*2\s*\)\s*{[\s\S]*?}/
-	);
+	game.menuZoom = 1;
 });
